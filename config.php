7cb060d04275d6d9386386a6547331cf841cb9b2
Edudotnexx <Edoudotnexx@outlook.com>
2025-02-04 15:32:14 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,24 +1,7 @@
 <?php
 
 $subscription_urls = [
-  "https://raw.githubusercontent.com/coldwater-10/V2rayCollectorLire/main/vmess_iran.txt",
+  "https://yebekhe.site/api/tce/api.php?id=Rayan_Proxy_Hub_I&base64=false",
   "https://raw.githubusercontent.com/coldwater-10/V2rayCollectorLire/main/ss_iran.txt",
-  "https://raw.githubusercontent.com/coldwater-10/V2rayCollectorLire/main/trojan_iran.txt",
-  "https://raw.githubusercontent.com/coldwater-10/V2rayCollectorLire/main/vless_iran.txt",
-  "https://dy.smjc.top/api/v1/client/subscribe?token=b12651a704cf680e5794f61827a46262",
-  "https://dy.smjc.top/api/v1/client/subscribe?token=5bd7e01f6a299be186e5cd44f9c6c150",
-  "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mci/sub_1.txt",
-  "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mci/sub_2.txt",
-  "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mci/sub_3.txt",
-  "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mci/sub_4.txt",
-  "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mtn/sub_1.txt",
-  "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mtn/sub_2.txt",
-  "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mtn/sub_3.txt",
-  "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mtn/sub_4.txt",
-  "https://raw.githubusercontent.com/V2RAYCONFIGSPOOL/V2RAY_SUB/main/V2RAY_SUB.txt",
-  "https://raw.githubusercontent.com/xc0000e9/deatnote/main/Hiddify-next.fragment",
-  "https://raw.githubusercontent.com/XYZMojtaba/sub/main/adsl.txt",
-  "https://raw.githubusercontent.com/XYZMojtaba/sub/main/mci.txt",
-  "https://raw.githubusercontent.com/XYZMojtaba/sub/main/mtn.txt",
-  "https://raw.githubusercontent.com/XYZMojtaba/sub/main/mtn2.txt",
+
 ];
