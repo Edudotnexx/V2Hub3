acd1d38c5d0385bae92f9560702c90db18f6ec60
Edudotnexx <Edoudotnexx@outlook.com>
2025-02-13 14:42:26 +0330

    Update config.php

--- config.php
+++ config.php
@@ -2,6 +2,7 @@
 
 $subscription_urls = [
   "https://yebekhe.site/api/tce/api.php?id=Rayan_Proxy_Hub_I&base64=false",
-  "https://raw.githubusercontent.com/coldwater-10/V2rayCollectorLire/main/ss_iran.txt",
+  "https://yebekhe.site/api/tce/api.php?id=oneclickvpnkeys&base64=false", 
+  "https://yebekhe.site/api/tce/api.php?id=mobilesignal&base64=false",
 
 ];
