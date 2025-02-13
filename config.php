816bee3068103bb5975907399548476c281270f0
Edudotnexx <Edoudotnexx@outlook.com>
2025-02-13 14:59:55 +0330

    Update config.php

--- config.php
+++ config.php
@@ -4,5 +4,12 @@ $subscription_urls = [
   "https://yebekhe.site/api/tce/api.php?id=Rayan_Proxy_Hub_I&base64=false",
   "https://yebekhe.site/api/tce/api.php?id=oneclickvpnkeys&base64=false", 
   "https://yebekhe.site/api/tce/api.php?id=mobilesignal&base64=false",
+  "https://yebekhe.site/api/tce/api.php?id=V2RAYNPLUS_GP&base64=false",
+  "https://yebekhe.site/api/tce/api.php?id=v2ray_sub&base64=false",
+  "https://yebekhe.site/api/tce/api.php?id=shadoowvpnn&base64=false",
+  "https://yebekhe.site/api/tce/api.php?id=v2rayntech&base64=false",
+  "https://yebekhe.site/api/tce/api.php?id=IP_CF_Configs&base64=false",
+  "https://yebekhe.site/api/tce/api.php?id=ShadowProxy66&base64=false",
+  "https://yebekhe.site/api/tce/api.php?id=Hope_Net&base64=false",
 
 ];
